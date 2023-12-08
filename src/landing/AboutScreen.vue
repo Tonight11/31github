1dc1c9d38739b52a4d65b1fd93c74bcd3bb0057b
tonight11 <godofspeedman@gmail.com>
2023-12-08 11:47:58 +0600

    feat: started about section

---
+++ src/landing/AboutScreen.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="about__inner">
+        <div class="about__info">
+          <div class="about__title title">Обо мне</div>
+          <div class="about__text">
+            На протяжении последних четырех лет занимаюсь разработкой
+            IT-продуктов. <br />
+            Бесконечно люблю Frontend во всех направлениях, нравится
+            воссоздавать наикрутещие дизайны, бизнес задачи и не только. <br />
+            Закончил Колледж им. Годовикова в Москве. Работал личным наставником
+            в области Frontend. <br />
+            Ценю красоту во всем, катаюсь на скейте, играю в футбол, учусь
+            дизайну, залипаю на небо и море. <br />
+            Нравится Figma, VS Code, Vut, Nuxt, Notion.
+          </div>
+        </div>
+        <div class="about__img">
+          <img src="@/assets/me.png" alt="me" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="content">
     <ProjectScreen />
+    <AboutScreen />
   </main>
 </template>
 
 <script setup>
   import ProjectScreen from "@/landing/ProjectScreen.vue";
+  import AboutScreen from "@/landing/AboutScreen.vue";
 </script>
 
 <style lang="scss">
