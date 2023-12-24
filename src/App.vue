2699430a07c4359ed35ead9f6e069009dc34c777
tonight11 <godofspeedman@gmail.com>
2023-12-24 21:40:52 +0600

    delete title animations

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <script setup>
   import TheHeader from "./components/TheHeader.vue";
   import TheFooter from "./components/TheFooter.vue";
-  import gsap from "gsap";
-  import ScrollTrigger from "gsap/ScrollTrigger";
-  gsap.registerPlugin(ScrollTrigger);
-
-  import { onMounted } from "vue";
-
-  const animateTitle = () => {
-    const tl = gsap.timeline();
-    tl.fromTo(
-      ".title",
-      {
-        scrollTrigger: {
-          trigger: ".title",
-          scrub: true,
-          start: "50% top",
-          markers: true,
-          invalidateOnResize: true,
-        },
-        opacity: 0,
-        y: -250,
-        autoAlpha: 0,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        autoAlpha: 1,
-        delay: 1,
-        duration: 1,
-        ease: "bounce.out",
-      },
-    );
-  };
-
-  onMounted(() => {
-    animateTitle();
-  });
 </script>
 
 <template>
